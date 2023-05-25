a65849b31359df59b472c8f84218017a702444f5
Alexander Riedel <54716527+alexriedel1@users.noreply.github.com>
2023-05-25 09:18:19 +0200

    nc

--- data/aligned_dataset.py
+++ data/aligned_dataset.py
@@ -26,8 +26,8 @@ class AlignedDataset(BaseDataset):
 
         assert (self.opt.load_size >= self.opt.crop_size
                 )  # crop_size should be smaller than the size of loaded image
-        self.input_nc = self.opt.output_nc if self.opt.direction == 'BtoA' else self.opt.input_nc
-        self.output_nc = self.opt.input_nc if self.opt.direction == 'BtoA' else self.opt.output_nc
+        self.input_nc = 3
+        self.output_nc = 3
         self.cache = {}
 
     def __getitem__(self, index):
