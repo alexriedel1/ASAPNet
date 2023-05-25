77e33b23ede3a10ae785b89d96abf61881929b37
Alexander Riedel <54716527+alexriedel1@users.noreply.github.com>
2023-05-25 09:27:39 +0200

    get transf

--- data/aligned_dataset.py
+++ data/aligned_dataset.py
@@ -43,11 +43,11 @@ class AlignedDataset(BaseDataset):
         # apply the same transform to both A and B
         transform_params = get_params(self.opt, A.size)
         A_transform = get_transform(self.opt,
-                                    transform_params,
-                                    grayscale=(self.input_nc == 1))
+                                    transform_params)
+                                    #grayscale=(self.input_nc == 1))
         B_transform = get_transform(self.opt,
-                                    transform_params,
-                                    grayscale=(self.output_nc == 1))
+                                    transform_params)
+                                    #grayscale=(self.output_nc == 1))
 
         A = A_transform(A)
         B = B_transform(B)
